c4da8249ad565d0d4c86e343632222ddb2f91503
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2021-12-21 12:39:18 -0500

    Update FloatExtensions.cs

--- Extensions/FloatExtensions.cs
+++ Extensions/FloatExtensions.cs
@@ -115,5 +115,66 @@ public static class FloatExtensions
     }
 
 
-    
+    public static float RoundToMultiple(this float input, float multiple, bool allowZero = true){
+        float halfwayCutoff = multiple / 2;
+
+        float remainderDown = input % multiple;
+        float remainderUp = multiple - remainderDown;
+
+        if(remainderDown < halfwayCutoff){
+            if(input - remainderDown == 0 && allowZero == false){
+                input += remainderUp;
+            }
+            else{
+                input -= remainderDown;
+            }        
+        }
+        else{
+            input += remainderUp;
+        }
+
+        return input;
+    }
+    public static Vector3 RoundToMultiple(this Vector3 inputs, Vector3 multiples){
+        Vector3 halfwayCutoffs = multiples / 2;
+
+        Vector3 remainderDown = new Vector3(inputs.x % multiples.x, inputs.y % multiples.y, inputs.z % multiples.z);
+        Vector3 remainderUp = multiples - remainderDown;
+
+        if(remainderDown.x < halfwayCutoffs.x){
+            inputs.x -= remainderDown.x;
+        }
+        else{
+            inputs.x = inputs.x + remainderUp.x;
+        }
+
+        if(remainderDown.y < halfwayCutoffs.y){
+            inputs.y -= remainderDown.y;
+        }
+        else{
+            inputs.y = inputs.y + remainderUp.y;
+        }
+
+        if(remainderDown.z < halfwayCutoffs.z){
+            inputs.z -= remainderDown.z;
+        }
+        else{
+            inputs.z = inputs.z + remainderUp.z;
+        }
+
+        return inputs;
+    }
+
+    public static float SignZero(this float input){
+        // Returns 0, 1, or -1 based on the sign or 0-ness of input
+        if (input > 0){
+            return 1;
+        }
+        else if (input < 0){
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
